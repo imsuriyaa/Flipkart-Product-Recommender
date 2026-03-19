1b474b278f0b8eb03206b63cd87adeca38862feb
imsuriyaa <suriyaa.iitkgp@gmail.com>
2026-03-19 11:48:25 +0530

    Update rag_chain's build_chain method to LCEL techniques

--- flipkart/rag_chain.py
+++ flipkart/rag_chain.py
@@ -1,10 +1,11 @@
+from operator import itemgetter
 from langchain_groq import ChatGroq
-from langchain.chains import create_history_aware_retriever,create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-# We are including this for the purpose of maintaining the history of the chat
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda
+# RunnableWithMessageHistory - handles updates of the history of the chat
 from langchain_core.runnables.history import RunnableWithMessageHistory
-# We are including this for the purpose of maintaining the history of the chat
+# ChatMessageHistory - maintains the history of the chat (kind of datatype)
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from flipkart.config import Config
@@ -18,7 +19,7 @@ class RAGChainBuilder:
     def _get_history(self,session_id:str) -> BaseChatMessageHistory:
         if session_id not in self.history_store:
             self.history_store[session_id] = ChatMessageHistory()
-        return self.history_store[session_id]
+        return self.history_dstore[session_id]
     
     def build_chain(self):
         retriever = self.vector_store.as_retriever(search_kwargs={"k":3})
@@ -36,24 +37,32 @@ class RAGChainBuilder:
             ("human", "{input}")  
         ])
 
-        history_aware_retriever = create_history_aware_retriever(
-            self.model , retriever , context_prompt
+        history_aware_retriever = context_prompt | self.model | StrOutputParser()
+
+        question_answer_chain = ({
+                    "context": itemgetter("input") | retriever,
+                    "input": itemgetter("input"),
+                    "chat_history": itemgetter("chat_history")
+                }
+                | qa_prompt
+                | self.model
+                | StrOutputParser()
         )
 
-        question_answer_chain = create_stuff_documents_chain(
-            self.model , qa_prompt
-        )
+        def rewrite_and_pass(input_dict):
+            rewritten = history_aware_retriever.invoke(input_dict)
+            return {
+                "input": rewritten,
+                "chat_history": input_dict["chat_history"]
+            }
 
-        rag_chain = create_retrieval_chain(
-            history_aware_retriever, question_answer_chain
-        )
+        rag_chain = RunnableLambda(rewrite_and_pass) | question_answer_chain
 
         return RunnableWithMessageHistory(
             rag_chain,
             self._get_history,
             input_messages_key="input",
-            history_messages_key="chat_history",
-            output_messages_key="answer"
+            history_messages_key="chat_history"
         )
 
 
